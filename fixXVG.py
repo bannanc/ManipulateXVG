af0ac1ce209eba68d8b48810645dd66407e61f57
Caitlin Bannan <bannanc@uci.edu>
2015-12-04 14:46:18 -0800

    add numpy, fixed examples

--- fixXVG.py
+++ fixXVG.py
@@ -1,37 +1,39 @@
 import os
+import numpy as np
 import commands as c
 
-def findDataStart(lines):
+def findDataStart(lines, delin = ' '):
     """
     Finds the line where the data starts
 
     input:
         lines = list of strings (probably from a data file)
-        delin = optional string, tells how the data would be separated
+        delin = optional string, tells how the data would be separated, default is a space (' ')
     
     output:
-        i = integer where the data stops being a string
-
+        i = integer where the data stops being a string (returns -1 if no data was found)
+        header = lines that make up the strings at the top of the file
+        datalines = lines starting from where the data ends
     """
     
-    for i, l in enumerate(lines, delin = ' '):
-        test = l.split(' ')[0]
+    for i, l in enumerate(lines):
+        test = l.split(delin)[0]
         # If it can't be turned into a float is is still a part of the header
         try:
             float(test)
-            return i, lines[i:]
+            return i, lines[0:i], lines[i:]
         except:
             continue
-    return -1, []
+    return -1, lines, []
 
-def removeCorruptLines(inputFN, outputFN = None, header = None, comments = ['#','@']):
+def removeCorruptLines(inputFN, outputFN = None, headerLine = None):
     """
     This method takes an .xvg file, removes any corrupted lines of data or data lines that are not sufficiently long. 
 
     input:
         inputFN - (String) name of input file
         outputFN - (optional, String) name of outputfile, will out output a file with the same name as input if not provided
-        header - (optional, int) number of lines at the beginning of the file consisting of strings, if None this will be found, if you know there is no header assign header = 0.
+        headerLine - (optional, int) number of lines at the beginning of the file consisting of strings, if None this will be found, if you know there is no header assign header = 0.
         comments = (optional, list of strings) characters that are used to indicate a comment in your file, default is '#' and '@' which are used in GROMACS xvg file headers. 
 
     output:
@@ -47,22 +49,42 @@ def removeCorruptLines(inputFN, outputFN = None, header = None, comments = ['#',
     if outputFN == None:
         outputFN = inputFN
 
-    # If you can read in file using np.loadtxt with comments then there is nothing wrong with the file.
-    try:
-        np.loadtxt(inputFN,comments = comments)
-        commands.getoutput('cp %s %s' % (inputFN outputFN))
-        print "Input File safe from corruption"
-        return 
-    except:
-        print "At least one line in inputfile needs to be removed"
-
-    # May not need this, researching numpy
-    if header == None:
+    # Need header lines for numpy.genfromtxt to work
+    # If not provided find it:
+    if headerLine == None:
         inputFile = open(inputFN, 'r')
-        header, datalines = findDataStart(inputFile.readlines())
+        headerLine, header, datalines = findDataStart(inputFile.readlines())
+        # Turn list of lines in header into a single string
+        header = ''.join(header)
         inputFile.close()
 
-     
+    # If headerLine was provided and it's bigger than zero get header:
+    elif headerLine > 0:
+        inputFile = open(inputFN,'r')
+        header = inputFile.readlines()[0:headerLine]
+        # Make header lines into string
+        header = ''.join(header)
+        inputFile.close()
+    else: # headerLine == 0:
+        header = '' # This is default for numpy.savetxt
+
+    # For writing header purposes we do not want the last character to create new line
+    if header[-1] == '\n':
+        header = header[:-1]
+
+    # Use genfromtxt to get data
+    # skip_header skips lines at the beginning of the file
+    # invalid_raise means that if a line is found with the wrong number of entries then it will be ignored and a warning will be printed
+    data = np.genfromtxt(inputFN, skip_header = headerLine, invalid_raise = False)
+
+    # Use numpy savetxt to write data (with header) to the output file
+    # fmt assigns the format of the float, for now it is the longest decimal in examples considered
+    # header prints the header from the original file to the top of the output
+    # comments adds that character to the beginning of ever line in the header
+    np.savetxt(outputFN, data, fmt='%.8f', header = header, comments = '')
+
+    print "file %s created with corrected data" % outputFN 
+    
 def removeCorruptLines_original(inputFN, outputFN = None):
     """
         Original version of this method, not removing until I'm convinced the new one works. 
@@ -80,7 +102,7 @@ def removeCorruptLines_original(inputFN, outputFN = None):
     inputFile.close()
 
     # Find where the data starts
-    end, dataLines = findDataStart(allLines)
+    end, header, dataLines = findDataStart(allLines)
     newlines = allLines[0:end]
     
     length = len(dataLines[0].split(' '))
@@ -111,13 +133,33 @@ def removeCorruptLines_original(inputFN, outputFN = None):
 
 # CombineXVG takes in two xvg files and combines data from the second one at the end of the first one starting at the last time in the first file
 def CombineXVG(input1, input2, output):
+    """
+    This method takes in two input .xvg files and adds the data from the second to the end of the first
+
+    input:
+        input1: first input file string.xvg
+        input2: data you want to add to the end of the first input
+        output: name of file for the combined data
+    
+    output:
+        returns lines that were written to output file
+        creates output file with combined data
+    """
     # clean up the input files
-    lines1 = removeCorruptLines_original(input1,'temp.xvg')
-    lines2 = removeCorruptLines_original(input2,'temp.xvg')
+    removeCorruptLines(input1)
+    removeCorruptLines(input2)
+
+    # Read in lines and close file
+    f1 = open(input1)
+    f2 = open(input2)
+    lines1 = f1.readlines()
+    lines2 = f2.readlines()
+    f1.close()
+    f2.close()
 
     # find where the data starts for both of them
-    end1, dataLines1 = findDataStart(lines1)
-    end2, dataLines2 = findDataStart(lines2)
+    end1, header1, dataLines1 = findDataStart(lines1)
+    end2, header2, dataLines2 = findDataStart(lines2)
 
     # time in ps that the first set of data ends
     endTime = float(dataLines1[-1].split(' ')[0])
@@ -132,14 +174,15 @@ def CombineXVG(input1, input2, output):
         try:
             startTime = float(datalines2[i].split(' ')[0])
         except:
-            print "No new data found in second input file"
             concat = False
 
     # combine data
     if concat:
         outputLines = lines1 + dataLines2[i:]
+        print "Files combined and written to ",output
     else:
         outputLines = lines1
+        print "No new data foudn in second input file"
 
     # write output file
     outFile = open(output,'w')
